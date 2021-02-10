22455235b2275dc36486fa69a81ab7d59f6c1c6a
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-09 16:22:05 -0800

    initial setup

---
+++ src/components/app/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import CharactersList from '../../containers/CharactersList';
+import CharacterDetails from '../../containers/CharacterDetails';
+
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={CharactersList} />
+        <Route path="/characters/:name" component={CharacterDetails} />
+      </Switch>
+    </Router>
+  );
+}
